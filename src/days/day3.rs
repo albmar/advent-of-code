8c4a61a056cbf189e022d462b700079d4f417ec2
albmar <ak.albmar@gmail.com>
2022-12-03 17:43:58 +0100

    Add solution for part 1 of day 3

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,17 +1,36 @@
-use crate::{solver::Solver, util::*};
+use crate::solver::Solver;
 
 pub struct Day3;
 
+fn get_priority(c: char) -> u32 {
+    c as u32
+        - if c.is_lowercase() {
+            'a' as u32 - 1
+        } else {
+            'A' as u32 - 27
+        }
+}
+
 impl<'a> Solver<'a> for Day3 {
-    type Parsed = u32;
+    type Parsed = Vec<&'a str>;
     type Output = u32;
 
     fn parse(input: &'a str) -> Self::Parsed {
-        todo!()
+        input.split("\n").collect()
     }
 
     fn part1(data: Self::Parsed) -> Self::Output {
-        todo!()
+        data.into_iter()
+            .map(|rucksack| {
+                let (first, second) = rucksack.split_at(rucksack.len() / 2);
+                first
+                    .chars()
+                    .filter(|&c| second.contains(c))
+                    .map(get_priority)
+                    .nth(0)
+                    .unwrap()
+            })
+            .sum()
     }
 
     fn part2(data: Self::Parsed) -> Self::Output {
@@ -24,8 +43,26 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_priority() {
+        assert_eq!(get_priority('a'), 1);
+        assert_eq!(get_priority('z'), 26);
+        assert_eq!(get_priority('A'), 27);
+        assert_eq!(get_priority('Z'), 52);
+    }
+
+    #[test]
     fn d3p1() {
-        assert_eq!(Day3::part1(Day3::parse("")), 0);
+        assert_eq!(
+            Day3::part1(Day3::parse(
+                "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"
+            )),
+            157
+        );
     }
 
     #[test]
