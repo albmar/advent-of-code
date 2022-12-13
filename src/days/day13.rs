e249e82f16c7b4aee3b7a927efafb082545a757f
albmar <ak.albmar@gmail.com>
2022-12-13 17:39:09 +0100

    Refactor day 13

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,11 +1,12 @@
-use std::{cmp::Ordering, collections::BTreeSet, fmt::Display};
+use std::{collections::BTreeSet, fmt::Display};
 
 use nom::{
     branch::alt,
     bytes::complete::tag,
     character::complete::{newline, u8},
+    combinator::map,
     multi::{separated_list0, separated_list1},
-    sequence::separated_pair,
+    sequence::{delimited, separated_pair},
     IResult,
 };
 
@@ -13,7 +14,7 @@ use crate::{solver::Solver, util::*};
 
 pub struct Day13;
 
-#[derive(Debug, Clone, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Packet {
     Int(u8),
     List(Vec<Packet>),
@@ -39,29 +40,6 @@ impl Display for Packet {
     }
 }
 
-impl From<u8> for Packet {
-    fn from(value: u8) -> Self {
-        Self::Int(value)
-    }
-}
-
-impl From<Vec<Packet>> for Packet {
-    fn from(value: Vec<Packet>) -> Self {
-        Self::List(value)
-    }
-}
-
-impl PartialEq for Packet {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Int(a), Self::Int(b)) => a == b,
-            (Self::List(a), Self::List(b)) => a == b,
-            (a, Packet::List(b)) => b.len() == 1 && a == &b[0],
-            (Packet::List(a), b) => a.len() == 1 && b == &a[0],
-        }
-    }
-}
-
 impl PartialOrd for Packet {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
@@ -72,59 +50,28 @@ impl Ord for Packet {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         match (self, other) {
             (Self::Int(a), Self::Int(b)) => a.cmp(b),
-            (Self::List(a), Self::List(b)) => {
-                for ord in a.iter().zip(b.iter()).map(|(a, b)| a.cmp(b)) {
-                    match ord {
-                        Ordering::Equal => (),
-                        _ => return ord,
-                    }
-                }
-                return a.len().cmp(&b.len());
-            }
-            (a, Packet::List(b)) => {
-                if let Some(b) = b.first() {
-                    match a.cmp(b) {
-                        Ordering::Equal => (),
-                        ord => return ord,
-                    }
-                }
-                return 1.cmp(&b.len());
-            }
-            (Packet::List(a), b) => {
-                if let Some(a) = a.first() {
-                    match a.cmp(b) {
-                        Ordering::Equal => (),
-                        ord => return ord,
-                    }
-                }
-                return a.len().cmp(&1);
-            }
+            (Self::List(a), Self::List(b)) => a.cmp(b),
+            (a, Packet::List(b)) => [a.clone()][..].cmp(b),
+            (Packet::List(a), b) => a[..].cmp(&[b.clone()][..]),
         }
     }
 }
 
-fn parse_list<'a>(i: &'a str) -> IResult<&'a str, Packet> {
-    let (i, _) = tag("[")(i)?;
-    let (i, list) = separated_list0(tag(","), parse_packet)(i)?;
-    let (i, _) = tag("]")(i)?;
-    Ok((i, list.into()))
-}
-
-fn parse_int<'a>(i: &'a str) -> IResult<&'a str, Packet> {
-    let (i, res) = u8(i)?;
-    Ok((i, res.into()))
-}
-
 fn parse_packet<'a>(i: &'a str) -> IResult<&'a str, Packet> {
-    alt((parse_int, parse_list))(i)
-}
-
-fn parse_pair<'a>(i: &'a str) -> IResult<&'a str, (Packet, Packet)> {
-    separated_pair(parse_packet, newline, parse_packet)(i)
+    alt((
+        map(u8, Packet::Int),
+        map(
+            delimited(tag("["), separated_list0(tag(","), parse_packet), tag("]")),
+            Packet::List,
+        ),
+    ))(i)
 }
 
 fn parse_input<'a>(i: &'a str) -> IResult<&'a str, Vec<(Packet, Packet)>> {
-    separated_list1(tag("\n\n"), parse_pair)(i)
+    separated_list1(
+        tag("\n\n"),
+        separated_pair(parse_packet, newline, parse_packet),
+    )(i)
 }
 
 impl<'a> Solver<'a> for Day13 {
@@ -136,13 +83,11 @@ impl<'a> Solver<'a> for Day13 {
     }
 
     fn part1(data: Self::Parsed) -> Self::Output {
-        let right_ordered = data
-            .iter()
-            .enumerate()
-            .map(|(i, x)| (i + 1, x))
-            .filter(|(_, (left, right))| left < right)
-            .collect::<Vec<_>>();
-        right_ordered.iter().map(|(i, _)| i).sum()
+        data.iter()
+            .zip(1..)
+            .filter(|((left, right), _)| left < right)
+            .map(|(_, i)| i)
+            .sum()
     }
 
     fn part2(data: Self::Parsed) -> Self::Output {
@@ -154,8 +99,8 @@ impl<'a> Solver<'a> for Day13 {
         let second = parse_packet("[[6]]").unwrap().1;
         packets.insert(first.clone());
         packets.insert(second.clone());
-        let first = dbg!(packets.iter().position(|x| x == &first)).unwrap() + 1;
-        let second = dbg!(packets.iter().position(|x| x == &second)).unwrap() + 1;
+        let first = packets.iter().position(|x| x == &first).unwrap() + 1;
+        let second = packets.iter().position(|x| x == &second).unwrap() + 1;
         first * second
     }
 }
