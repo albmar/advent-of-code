027fc60d2bf13b2c85b161bdf806bbaf4e9133ce
albmar <ak.albmar@gmail.com>
2022-12-15 18:24:41 +0100

    Add solution of part 1 for day 15

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -46,7 +46,7 @@ fn main() {
     day!(12, 504, 500);
     day!(13, 5506, 21756);
     day!(14, 1003, 25771);
-    day!(15);
+    day!(15, 5511201);
 }
 
 fn solve<O, O2, S: for<'a> Solver<'a, Output = O, Output2 = O2>>(
--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,17 +1,67 @@
+use nalgebra::Vector2;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, newline},
+    combinator::map,
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
+
 use crate::{solver::Solver, util::*};
 
 pub struct Day15;
 
+fn parse_input<'a>(i: &'a str) -> IResult<&'a str, Vec<(Vector2<i32>, Vector2<i32>)>> {
+    separated_list1(
+        newline,
+        separated_pair(
+            map(
+                separated_pair(
+                    preceded(tag("Sensor at x="), complete::i32),
+                    tag(", y="),
+                    complete::i32,
+                ),
+                |pair| Vector2::new(pair.0, pair.1),
+            ),
+            tag(": closest beacon is at x="),
+            map(
+                separated_pair(complete::i32, tag(", y="), complete::i32),
+                |pair| Vector2::new(pair.0, pair.1),
+            ),
+        ),
+    )(i)
+}
+
 impl<'a> Solver<'a> for Day15 {
-    type Parsed = u32;
-    type Output = u32;
+    type Parsed = Vec<(Vector2<i32>, Vector2<i32>)>;
+    type Output = usize;
 
     fn parse(input: &'a str) -> Self::Parsed {
-        todo!()
+        parse_input(input).unwrap().1
     }
 
     fn part1(data: Self::Parsed) -> Self::Output {
-        todo!()
+        let sensor_distances = data
+            .iter()
+            .map(|(sensor, beacon)| (sensor, (sensor - beacon).abs().sum()))
+            .collect::<Vec<_>>();
+        let min_x = sensor_distances.iter().map(|(s, d)| s.x - d).min().unwrap();
+        let max_x = sensor_distances.iter().map(|(s, d)| s.x + d).max().unwrap();
+        let y = if data.first().unwrap().0.x == 2 {
+            10
+        } else {
+            2_000_000
+        };
+        (min_x..=max_x)
+            .map(move |x| Vector2::new(x, y))
+            .filter(|v| data.iter().all(|(s, b)| v != s && v != b))
+            .filter(|v| {
+                sensor_distances
+                    .iter()
+                    .any(|&(s, d)| (v - s).abs().sum() <= d)
+            })
+            .count()
     }
 
     fn part2(data: Self::Parsed) -> Self::Output {
@@ -25,7 +75,26 @@ mod tests {
 
     #[test]
     fn d15p1() {
-        assert_eq!(Day15::part1(Day15::parse("")), 0);
+        assert_eq!(
+            Day15::part1(Day15::parse(
+                "Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+"
+            )),
+            26
+        );
     }
 
     #[test]
